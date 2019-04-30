3b2f0774c56720ce11b290f47e7ede23cc85ec64
charlychiu <charlychiu@gmail.com>
2019-04-30 22:17:20 +0800

    Update readme & script

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Laravel-Deploy-shellscript
+
+## Auto deploy script
+> ./autoDeploy.sh `<repo>` `<wanted-name-of-project>` `<DB-database>` `<DB-username>` `<DB-password>`
+
+## Renew online project script
+> ./renewDeploy.sh `<path-to-project>`
--- autoDeploy.sh
+++ autoDeploy.sh
@@ -7,34 +7,21 @@ composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
 
 cp .env.example .env
 
+# config setting to production usage
 sed -i "" 's/APP_ENV=local/APP_ENV=production/g' .env
 sed -i "" 's/APP_DEBUG=true/APP_DEBUG=false/g' .env
 sed -i "" 's/DB_DATABASE=homestead/DB_DATABASE='$3'/g' .env
 sed -i "" 's/DB_USERNAME=homestead/DB_USERNAME='$4'/g' .env
 sed -i "" 's/DB_PASSWORD=secret/DB_PASSWORD='$5'/g' .env
 
+# generate application key
 php artisan key:generate
 
 # Run database migrations
 php artisan migrate --force --seed
-
-# Clear caches
-php artisan cache:clear
-
-# Clear expired password reset tokens
-php artisan auth:clear-resets
-
-# Clear and cache routes
-php artisan route:clear
-php artisan route:cache
-
-# Clear and cache config
-php artisan config:clear
-php artisan config:cache
 
 # Install node modules
 npm install
 
 # Build assets using Laravel Mix
 npm run production
-
